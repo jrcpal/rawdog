6dfb6eccbe26a644d4dfa24ce14b079d2a13151e
Jess <jchurgy@gmail.com>
2025-12-12 11:37:32 -0500

    add lucide and capitalize text via tailwind

--- apps/react/p1-dropdown/app/page.tsx
+++ apps/react/p1-dropdown/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import items from "./groceryItems";
 import React, { useState } from "react";
+import { ChevronDown } from "lucide-react";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,26 +11,25 @@ export default function Home() {
   };
 
   return (
-    <div className="flex min-h-screen items-start justify-left bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-start justify-left bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs mb-6 text-3xl font-semibold leading-10 tracking-tight text-black ">
             Simple Dropdown
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            React nested dropdown displaying grocery items
-          </p>
         </div>
         <div className="dropdown h-20 w-2xl flex flex-col items-start">
-          <label>Grocery Items</label>
+          <label>React nested dropdown displaying grocery items</label>
           <button
-            className="dropdown-btn w-lg text-left border border-solid border-black"
+            className="dropdown-btn mt-1 place-content-between flex w-lg text-left border border-solid border-black"
             onClick={() => toggleDropdown()}
           >
             Categories
+            <ChevronDown />
           </button>
+          
           {items && isOpen && (
-            <div id="groeceryDropdown" className="dropdown=content">
+            <div id="groeceryDropdown" className="dropdown=content capitalize">
               <ul>
                 {Object.keys(items).map((category) => (
                   <li key={category}>{category}</li>
